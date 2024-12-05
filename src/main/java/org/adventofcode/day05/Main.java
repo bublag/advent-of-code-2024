e31485c8b07d948b397bd29d6c57b9e624d330c5
Gabor Bubla <gabor.bubla@dmgmori-heitec.hu>
2024-12-05 20:57:13 +0100

    2024 Day05Part1 solution

--- src/main/java/org/adventofcode/day05/Main.java
+++ src/main/java/org/adventofcode/day05/Main.java
@@ -6,7 +6,13 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @UtilityClass
 public class Main {
@@ -18,6 +24,71 @@ public class Main {
 			StandardCharsets.UTF_8
 		);
 
-		// TODO
+		// saving the first section, "page ordering rules" into a Map
+		// saving the second section, "page numbers of each update" to a List
+		final Map<Integer, Set<Integer>> pageOrderingRules = new HashMap<>();
+		final List<List<Integer>> updates = new ArrayList<>();
+		for (final String inputByLine : inputByLines) {
+			if (inputByLine.contains("|")) {
+				final String[] split = inputByLine.split("\\|");
+				final int leftNumber = Integer.parseInt(split[0]);
+				final int rightNumber = Integer.parseInt(split[1]);
+				if (pageOrderingRules.containsKey(leftNumber)) {
+					pageOrderingRules.get(leftNumber)
+						.add(rightNumber);
+				} else {
+					final Set<Integer> rulesForLeftNumber = new HashSet<>();
+					rulesForLeftNumber.add(rightNumber);
+					pageOrderingRules.put(leftNumber, rulesForLeftNumber);
+				}
+			}
+			if (inputByLine.contains(",")) {
+				updates.add(
+					Arrays.stream(inputByLine.split(","))
+						.map(Integer::parseInt)
+						.toList()
+				);
+			}
+		}
+
+		/*
+		Go over the updates and consider them if they are valid (the order of the numbers inside them are valid
+		considering the page ordering rules).
+		If an update is valid, get the number in the middle and sum all those up.
+		 */
+		long validUpdatesMiddleNumberSum = 0;
+		for (final List<Integer> update : updates) {
+			// not validating the first number
+			boolean isUpdateValid = true;
+			for (int i = 1; i < update.size(); i++) {
+				final Integer currentNumberToValidate = update.get(i);
+				final Set<Integer> rulesForCurrentNumber = pageOrderingRules.get(currentNumberToValidate);
+				// go back in the list of numbers and check if there is any number before this which cant be based on the rules
+				boolean isNumberValid = true;
+				for (int j = i - 1; j >= 0 ; j--) {
+					final Integer previousNumber = update.get(j);
+					if (rulesForCurrentNumber.contains(previousNumber)) {
+						isNumberValid = false;
+						break;
+					}
+				}
+				if (!isNumberValid) {
+					isUpdateValid = false;
+					break;
+				}
+			}
+			if (isUpdateValid) {
+				validUpdatesMiddleNumberSum += getMiddleNumber(update);
+			}
+		}
+		System.out.println("validUpdatesMiddleNumberSum = " + validUpdatesMiddleNumberSum);
+	}
+
+	private static int getMiddleNumber(final List<Integer> update) {
+		if (update.size() % 2 == 0) {
+			throw new UnsupportedOperationException("The size of the update is even. Can't pick the middle number if there are 2 middle numbers. The update: " + update);
+		} else {
+		    return update.get((update.size() / 2));
+		}
 	}
 }
