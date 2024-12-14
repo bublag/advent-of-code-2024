4383c8e83eebc9e9b6b76233c1cb007bb70579af
Gabor Bubla <gabor.bubla@dmgmori-heitec.hu>
2024-12-14 16:05:54 +0100

    2024 Day12Part2 solution

--- src/main/java/org/adventofcode/day12/Main.java
+++ src/main/java/org/adventofcode/day12/Main.java
@@ -70,6 +70,41 @@ public class Main {
 		System.out.println("part1 solution runtime in milliseconds = " + (System.currentTimeMillis() - start));
 		System.out.println("totalPriceOfFencingRegions = " + totalPriceOfFencingRegions);
 		System.out.println();
+
+		/*
+		part 2: instead of calculating the perimeters, we need to count the sides of the regions
+		"Each straight section of fence counts as a side, regardless of how long it is."
+		The regions' number of sides are equal to their number of corners. We are counting the corners instead.
+		Every "inside" and "outside" corner has to be calculated. I will call a corner "inside" corner if 1 plant is
+		inside the corner. I will call a corner "outside" if 3 plants are in the region, but the 1 plant in the corner
+		is a different plant in a different region.
+		 */
+		// go over every point and build up a list of regions and calculate their corners (=sides) at the same time
+		final long start2 = System.currentTimeMillis();
+		final Set<Point> visitedPoints2 = new LinkedHashSet<>();
+		final List<RegionWithSides> regionWithSides = new ArrayList<>();
+		for (int y = 0; y < mapMaxY + 1; y++) {
+			for (int x = 0; x < mapMaxX + 1; x++) {
+				final Point currentPoint = new Point(x, y);
+				if (visitedPoints2.contains(currentPoint)) {
+					continue;
+				}
+				final Set<Point> visitedPointsWithinRegion = new LinkedHashSet<>();
+				final char plant = map[y][x];
+				final int corners = buildRegionWithSidesWithDepthFirstSearch(map, mapMaxX, mapMaxY, currentPoint, plant, visitedPointsWithinRegion, 0);
+				visitedPoints2.addAll(visitedPointsWithinRegion);
+				regionWithSides.add(new RegionWithSides(visitedPointsWithinRegion, plant, corners));
+			}
+		}
+
+		// price of fence required for a region is found by multiplying that region's area by its perimeter
+		// count the total price of fencing all the regions
+		long totalPriceOfFencingRegionsWithDiscount = 0;
+		for (final RegionWithSides region : regionWithSides) {
+			totalPriceOfFencingRegionsWithDiscount += ((long) region.plantPoints().size()) * region.sides();
+		}
+		System.out.println("part2 solution runtime in milliseconds = " + (System.currentTimeMillis() - start2));
+		System.out.println("totalPriceOfFencingRegionsWithDiscount = " + totalPriceOfFencingRegionsWithDiscount);
 	}
 
 	private static void printMap(final char[][] map) {
@@ -82,12 +117,17 @@ public class Main {
 		System.out.println();
 	}
 
+	/**
+	 * Go "deeper" in the graph recursively in all 4 directions, if the neighbor location is the same plant as the
+	 * current (and inside map bounds). If we cant go in a direction, that means a fence has to be put there so we
+	 * increase the perimeter by 1.
+	 */
 	private static int buildRegionWithDepthFirstSearch(
 		final char[][] map,
 		final int mapMaxX,
 		final int mapMaxY,
 		final Point currentPoint,
-		final char plant,
+		final char currentPlant,
 		final Set<Point> visitedPointsWithinRegion,
 		final int currentPerimeter
 	) {
@@ -96,12 +136,10 @@ public class Main {
 		}
 		visitedPointsWithinRegion.add(currentPoint);
 		int newCurrentPerimeter = currentPerimeter;
-		// go "deeper" in the graph recursively in all 4 directions, if the neighbor location is the same plant as the current (and inside map bounds)
-		// if we cant go in a direction, that means a fence has to be put there so we increase the perimeter by 1
 		if (currentPoint.y() > 0) { // 1 up is still within bounds
 			final Point pointToOneUp = new Point(currentPoint.x(), currentPoint.y() - 1);
-			if (map[pointToOneUp.y()][pointToOneUp.x()] == plant) {
-				newCurrentPerimeter = buildRegionWithDepthFirstSearch(map, mapMaxX, mapMaxY, pointToOneUp, plant, visitedPointsWithinRegion, newCurrentPerimeter);
+			if (map[pointToOneUp.y()][pointToOneUp.x()] == currentPlant) {
+				newCurrentPerimeter = buildRegionWithDepthFirstSearch(map, mapMaxX, mapMaxY, pointToOneUp, currentPlant, visitedPointsWithinRegion, newCurrentPerimeter);
 			} else {
 			    newCurrentPerimeter++;
 			}
@@ -110,8 +148,8 @@ public class Main {
 		}
 		if (currentPoint.x() < mapMaxX) { // 1 right is still within bounds
 			final Point pointToOneRight = new Point(currentPoint.x() + 1, currentPoint.y());
-			if (map[pointToOneRight.y()][pointToOneRight.x()] == plant) {
-				newCurrentPerimeter = buildRegionWithDepthFirstSearch(map, mapMaxX, mapMaxY, pointToOneRight, plant, visitedPointsWithinRegion, newCurrentPerimeter);
+			if (map[pointToOneRight.y()][pointToOneRight.x()] == currentPlant) {
+				newCurrentPerimeter = buildRegionWithDepthFirstSearch(map, mapMaxX, mapMaxY, pointToOneRight, currentPlant, visitedPointsWithinRegion, newCurrentPerimeter);
 			} else {
 				newCurrentPerimeter++;
 			}
@@ -120,8 +158,8 @@ public class Main {
 		}
 		if (currentPoint.y() < mapMaxY) { // 1 down is still within bounds
 			final Point pointToOneDown = new Point(currentPoint.x(), currentPoint.y() + 1);
-			if (map[pointToOneDown.y()][pointToOneDown.x()] == plant) {
-				newCurrentPerimeter = buildRegionWithDepthFirstSearch(map, mapMaxX, mapMaxY, pointToOneDown, plant, visitedPointsWithinRegion, newCurrentPerimeter);
+			if (map[pointToOneDown.y()][pointToOneDown.x()] == currentPlant) {
+				newCurrentPerimeter = buildRegionWithDepthFirstSearch(map, mapMaxX, mapMaxY, pointToOneDown, currentPlant, visitedPointsWithinRegion, newCurrentPerimeter);
 			} else {
 				newCurrentPerimeter++;
 			}
@@ -130,8 +168,8 @@ public class Main {
 		}
 		if (currentPoint.x() > 0) { // 1 left is still within bounds
 			final Point pointToOneLeft = new Point(currentPoint.x() - 1, currentPoint.y());
-			if (map[pointToOneLeft.y()][pointToOneLeft.x()] == plant) {
-				newCurrentPerimeter = buildRegionWithDepthFirstSearch(map, mapMaxX, mapMaxY, pointToOneLeft, plant, visitedPointsWithinRegion, newCurrentPerimeter);
+			if (map[pointToOneLeft.y()][pointToOneLeft.x()] == currentPlant) {
+				newCurrentPerimeter = buildRegionWithDepthFirstSearch(map, mapMaxX, mapMaxY, pointToOneLeft, currentPlant, visitedPointsWithinRegion, newCurrentPerimeter);
 			} else {
 				newCurrentPerimeter++;
 			}
@@ -141,7 +179,176 @@ public class Main {
 		return newCurrentPerimeter;
 	}
 
+	/**
+	 * Go "deeper" in the graph recursively in all 4 directions, if the neighbor location is the same plant as the
+	 * current (and inside map bounds). We have to count the "inside" and the "outside" corners the region has.
+	 */
+	private static int buildRegionWithSidesWithDepthFirstSearch(
+		final char[][] map,
+		final int mapMaxX,
+		final int mapMaxY,
+		final Point currentPoint,
+		final char currentPlant,
+		final Set<Point> visitedPointsWithinRegion,
+		final int currentCorners
+	) {
+		if (visitedPointsWithinRegion.contains(currentPoint)) {
+			return currentCorners;
+		}
+		visitedPointsWithinRegion.add(currentPoint);
+		int newCurrentCorners = currentCorners;
+		if (checkCornersTopRight(map, mapMaxX, mapMaxY, currentPoint, currentPlant)) {
+			newCurrentCorners++;
+		}
+		if (checkCornersBottomRight(map, mapMaxX, mapMaxY, currentPoint, currentPlant)) {
+			newCurrentCorners++;
+		}
+		if (checkCornersBottomLeft(map, mapMaxX, mapMaxY, currentPoint, currentPlant)) {
+			newCurrentCorners++;
+		}
+		if (checkCornersTopLeft(map, mapMaxX, mapMaxY, currentPoint, currentPlant)) {
+			newCurrentCorners++;
+		}
+		if (currentPoint.y() > 0) { // 1 up is still within bounds
+			final Point pointToOneUp = new Point(currentPoint.x(), currentPoint.y() - 1);
+			if (map[pointToOneUp.y()][pointToOneUp.x()] == currentPlant) {
+				newCurrentCorners = buildRegionWithSidesWithDepthFirstSearch(map, mapMaxX, mapMaxY, pointToOneUp, currentPlant, visitedPointsWithinRegion, newCurrentCorners);
+			}
+		}
+		if (currentPoint.x() < mapMaxX) { // 1 right is still within bounds
+			final Point pointToOneRight = new Point(currentPoint.x() + 1, currentPoint.y());
+			if (map[pointToOneRight.y()][pointToOneRight.x()] == currentPlant) {
+				newCurrentCorners = buildRegionWithSidesWithDepthFirstSearch(map, mapMaxX, mapMaxY, pointToOneRight, currentPlant, visitedPointsWithinRegion, newCurrentCorners);
+			}
+		}
+		if (currentPoint.y() < mapMaxY) { // 1 down is still within bounds
+			final Point pointToOneDown = new Point(currentPoint.x(), currentPoint.y() + 1);
+			if (map[pointToOneDown.y()][pointToOneDown.x()] == currentPlant) {
+				newCurrentCorners = buildRegionWithSidesWithDepthFirstSearch(map, mapMaxX, mapMaxY, pointToOneDown, currentPlant, visitedPointsWithinRegion, newCurrentCorners);
+			}
+		}
+		if (currentPoint.x() > 0) { // 1 left is still within bounds
+			final Point pointToOneLeft = new Point(currentPoint.x() - 1, currentPoint.y());
+			if (map[pointToOneLeft.y()][pointToOneLeft.x()] == currentPlant) {
+				newCurrentCorners = buildRegionWithSidesWithDepthFirstSearch(map, mapMaxX, mapMaxY, pointToOneLeft, currentPlant, visitedPointsWithinRegion, newCurrentCorners);
+			}
+		}
+		return newCurrentCorners;
+	}
+
+	/**
+	 * We find an "inside" corner, if both 2 neighbors (for example up+right) next to the point have different plant.
+	 * We find an "outside" corner, if both 2 neighbors next to the point are the same plant, and the diagonal point in
+	 * that direction has a different plant. We only check for "outside" corner where there is no "inside" corner in
+	 * that diagonal direction.
+	 */
+	private static boolean checkCornersTopRight(
+		final char[][] map,
+		final int mapMaxX,
+		final int mapMaxY,
+		final Point currentPoint,
+		final char currentPlant
+	) {
+		final boolean isTopWithinBounds = currentPoint.y() > 0;
+		final Point pointToOneUp = new Point(currentPoint.x(), currentPoint.y() - 1);
+		final boolean isTopTheSamePlant = isTopWithinBounds && map[pointToOneUp.y()][pointToOneUp.x()] == currentPlant;
+
+		final boolean isRightWithinBounds = currentPoint.x() < mapMaxX;
+		final Point pointToOneRight = new Point(currentPoint.x() + 1, currentPoint.y());
+		final boolean isRightTheSamePlant = isRightWithinBounds && map[pointToOneRight.y()][pointToOneRight.x()] == currentPlant;
+
+		if (!isTopTheSamePlant && !isRightTheSamePlant) {
+			return true;
+		} else if (isTopTheSamePlant && isRightTheSamePlant) {
+			final Point pointToTopRightDiagonal = new Point(currentPoint.x() + 1, currentPoint.y() - 1);
+			return map[pointToTopRightDiagonal.y()][pointToTopRightDiagonal.x()] != currentPlant; // not the same plant -> "outside corner"
+		}
+		return false;
+	}
+
+	/**
+	 * @see #checkCornersTopRight(char[][], int, int, Point, char)
+	 */
+	private static boolean checkCornersBottomRight(
+		final char[][] map,
+		final int mapMaxX,
+		final int mapMaxY,
+		final Point currentPoint,
+		final char currentPlant
+	) {
+		final boolean isBottomWithinBounds = currentPoint.y() < mapMaxY;
+		final Point pointToOneDown = new Point(currentPoint.x(), currentPoint.y() + 1);
+		final boolean isBottomTheSamePlant = isBottomWithinBounds && map[pointToOneDown.y()][pointToOneDown.x()] == currentPlant;
+
+		final boolean isRightWithinBounds = currentPoint.x() < mapMaxX;
+		final Point pointToOneRight = new Point(currentPoint.x() + 1, currentPoint.y());
+		final boolean isRightTheSamePlant = isRightWithinBounds && map[pointToOneRight.y()][pointToOneRight.x()] == currentPlant;
+
+		if (!isBottomTheSamePlant && !isRightTheSamePlant) {
+			return true;
+		} else if (isBottomTheSamePlant && isRightTheSamePlant) {
+			final Point pointToBottomRightDiagonal = new Point(currentPoint.x() + 1, currentPoint.y() + 1);
+			return map[pointToBottomRightDiagonal.y()][pointToBottomRightDiagonal.x()] != currentPlant; // not the same plant -> "outside corner"
+		}
+		return false;
+	}
+
+	/**
+	 * @see #checkCornersTopRight(char[][], int, int, Point, char)
+	 */
+	private static boolean checkCornersBottomLeft(
+		final char[][] map,
+		final int mapMaxX,
+		final int mapMaxY,
+		final Point currentPoint,
+		final char currentPlant
+	) {
+		final boolean isBottomWithinBounds = currentPoint.y() < mapMaxY;
+		final Point pointToOneDown = new Point(currentPoint.x(), currentPoint.y() + 1);
+		final boolean isBottomTheSamePlant = isBottomWithinBounds && map[pointToOneDown.y()][pointToOneDown.x()] == currentPlant;
+
+		final boolean isLeftWithinBounds = currentPoint.x() > 0;
+		final Point pointToOneLeft = new Point(currentPoint.x() - 1, currentPoint.y());
+		final boolean isLeftTheSamePlant = isLeftWithinBounds && map[pointToOneLeft.y()][pointToOneLeft.x()] == currentPlant;
+
+		if (!isBottomTheSamePlant && !isLeftTheSamePlant) {
+			return true;
+		} else if (isBottomTheSamePlant && isLeftTheSamePlant) {
+			final Point pointToBottomLeftDiagonal = new Point(currentPoint.x() - 1, currentPoint.y() + 1);
+			return map[pointToBottomLeftDiagonal.y()][pointToBottomLeftDiagonal.x()] != currentPlant; // not the same plant -> "outside corner"
+		}
+		return false;
+	}
+
+	/**
+	 * @see #checkCornersTopRight(char[][], int, int, Point, char)
+	 */
+	private static boolean checkCornersTopLeft(
+		final char[][] map,
+		final int mapMaxX,
+		final int mapMaxY,
+		final Point currentPoint,
+		final char currentPlant
+	) {
+		final boolean isTopWithinBounds = currentPoint.y() > 0;
+		final Point pointToOneUp = new Point(currentPoint.x(), currentPoint.y() - 1);
+		final boolean isTopTheSamePlant = isTopWithinBounds && map[pointToOneUp.y()][pointToOneUp.x()] == currentPlant;
+
+		final boolean isLeftWithinBounds = currentPoint.x() > 0;
+		final Point pointToOneLeft = new Point(currentPoint.x() - 1, currentPoint.y());
+		final boolean isLeftTheSamePlant = isLeftWithinBounds && map[pointToOneLeft.y()][pointToOneLeft.x()] == currentPlant;
+
+		if (!isTopTheSamePlant && !isLeftTheSamePlant) {
+			return true;
+		} else if (isTopTheSamePlant && isLeftTheSamePlant) {
+			final Point pointToTopRightDiagonal = new Point(currentPoint.x() - 1, currentPoint.y() - 1);
+			return map[pointToTopRightDiagonal.y()][pointToTopRightDiagonal.x()] != currentPlant; // not the same plant -> "outside corner"
+		}
+		return false;
+	}
+
 	private record Point(int x, int y) {}
 
 	private record Region(Set<Point> plantPoints, char plant, int perimeter) {}
+	private record RegionWithSides(Set<Point> plantPoints, char plant, int sides) {}
 }
